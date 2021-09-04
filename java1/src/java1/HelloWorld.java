7119f4989d7c8609e723bdd3c4b5605b6f4c25da
ThanhTu0301 <89456038+ThanhTu0301@users.noreply.github.com>
2021-09-04 22:13:38 +0700

    Update HelloWorld.java

--- java1/src/java1/HelloWorld.java
+++ java1/src/java1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello world");
+		System.out.print("Ví dụ họ sửa code ở đây, mình phải pull về máy lại/ à hiểu rùi nè");
 	}
 
 }
